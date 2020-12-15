3dfa476dc9443de105a439702a7f7a51bceb353b
dawidor <dr4aa@hotmail.com>
2020-12-15 12:44:40 +0100

    runbook

---
+++ vm-list2.ps1
@@ -0,0 +1,19 @@
+Workflow VMs-List-Workflow
+{
+  Write-Output "starting..."
+
+  $VMs = GET-AzureVM
+
+  $SUBSC = Get-AzureSubscription
+
+  Write-Output "Subs: $SUBSC"
+
+  if (!$VMs) {
+      Write-Output "No VMs found"
+  } else {
+      Write-Output $VMs[0..9]
+  }
+
+  Write-Output "Ending..."
+}
+
